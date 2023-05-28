c234674b61e4a53adadbaf369dbc6e1e4f2c73a8
lowee1 <63391242+lowee1@users.noreply.github.com>
2023-05-28 21:37:12 +0800

    bare minimum routes

--- carbon-footprint-pwa/src/App.js
+++ carbon-footprint-pwa/src/App.js
@@ -1,16 +1,57 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path="/"></Route>
-        <Route path="/Forest"></Route>
-        <Route path="/Statistics"></Route>
-      </Switch>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/Forest">Forest</Link>
+          </li>
+          <li>
+            <Link to="/Statistics">Statistics</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/Forest" element={<ForestPage />} />
+        <Route path="/Statistics" element={<StatisticsPage />} />
+      </Routes>
     </BrowserRouter>
+  );
+}
+
+function HomePage() {
+  return (
+    <>
+      <h1>Home page</h1>
+      <Link to="/Forest">Forest</Link>
+      <Link to="/Statistics">Statistics</Link>
+    </>
+  );
+}
+
+function ForestPage() {
+  return (
+    <>
+      <h1>Forest</h1>
+      <Link to="/">Home page</Link>
+      <Link to="/Statistics">Statistics</Link>
+    </>
+  );
+}
+
+function StatisticsPage() {
+  return (
+    <>
+      <h1>Statistics</h1>
+      <Link to="/">Home page</Link>
+      <Link to="/Forest">Forest</Link>
+    </>
   );
 }
 
